fb8ab037f00ffa45d652fb9521ec9ff72c58bede
Jud1714 <juandiegolopezarias07@gmail.com>
2026-03-02 11:16:19 -0500

    Add main application class for Spring Boot initialization

---
+++ src/main/java/com/peciatech/alomediabackend/AlomediabackendApplication.java
@@ -0,0 +1,13 @@
+package com.peciatech.alomediabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlomediabackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlomediabackendApplication.class, args);
+	}
+
+}
