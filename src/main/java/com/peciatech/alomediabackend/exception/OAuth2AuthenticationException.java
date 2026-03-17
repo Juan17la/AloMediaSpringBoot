e1aadc9bc772e9dc62ba0b6c8cbef763754c3d83
Jud1714 <juandiegolopezarias07@gmail.com>
2026-03-17 09:45:35 -0500

    Add AuthController for user authentication and password recovery

---
+++ src/main/java/com/peciatech/alomediabackend/exception/OAuth2AuthenticationException.java
@@ -0,0 +1,8 @@
+package com.peciatech.alomediabackend.exception;
+
+public class OAuth2AuthenticationException extends RuntimeException {
+
+    public OAuth2AuthenticationException(String message) {
+        super(message);
+    }
+}
