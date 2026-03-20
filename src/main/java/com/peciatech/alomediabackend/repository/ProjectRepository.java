333545ed2f7274d9141c0810f8f696de99e07a75
Jud1714 <juandiegolopezarias07@gmail.com>
2026-03-19 19:28:36 -0500

    Add findByOwner method to ProjectRepository for querying projects by owner

--- src/main/java/com/peciatech/alomediabackend/repository/ProjectRepository.java
+++ src/main/java/com/peciatech/alomediabackend/repository/ProjectRepository.java
@@ -1,6 +1,7 @@
 package com.peciatech.alomediabackend.repository;
 
 import com.peciatech.alomediabackend.entity.Project;
+import com.peciatech.alomediabackend.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,6 +11,8 @@ import java.util.List;
 public interface ProjectRepository extends JpaRepository<Project, Long> {
 
     List<Project> findByOwnerId(Long ownerId);
+
+    Page<Project> findByOwner(User owner, Pageable pageable);
 
     Page<Project> findByOwnerId(Long ownerId, Pageable pageable);
 
