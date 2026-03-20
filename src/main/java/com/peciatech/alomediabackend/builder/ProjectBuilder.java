96ec863f310accb8a1173af1c1eca8d45d99f509
Jud1714 <juandiegolopezarias07@gmail.com>
2026-03-19 19:28:07 -0500

    Add ProjectBuilder class for constructing Project instances

---
+++ src/main/java/com/peciatech/alomediabackend/builder/ProjectBuilder.java
@@ -0,0 +1,42 @@
+package com.peciatech.alomediabackend.builder;
+
+import com.peciatech.alomediabackend.entity.Project;
+import com.peciatech.alomediabackend.entity.User;
+import com.peciatech.alomediabackend.enums.ProjectStatus;
+
+public class ProjectBuilder {
+
+    private String name;
+    private String description;
+    private String timelineData;
+    private User owner;
+
+    public ProjectBuilder setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public ProjectBuilder setDescription(String description) {
+        this.description = description;
+        return this;
+    }
+
+    public ProjectBuilder setTimelineData(String timelineData) {
+        this.timelineData = timelineData;
+        return this;
+    }
+
+    public ProjectBuilder setOwner(User owner) {
+        this.owner = owner;
+        return this;
+    }
+
+    public Project build() {
+        Project project = new Project();
+        project.setName(name);
+        project.setTimelineData(timelineData != null ? timelineData : "{\"version\":1,\"tracks\":[],\"media\":[]}");
+        project.setOwner(owner);
+        project.setStatus(ProjectStatus.DRAFT);
+        return project;
+    }
+}
