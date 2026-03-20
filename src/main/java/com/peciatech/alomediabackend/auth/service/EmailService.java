00e541dde7693e78fd93280ea145ed4941c022d4
Jud1714 <juandiegolopezarias07@gmail.com>
2026-03-19 20:38:57 -0500

    Add AuthService, EmailService, and OAuth2Service for user authentication and password recovery

---
+++ src/main/java/com/peciatech/alomediabackend/auth/service/EmailService.java
@@ -0,0 +1,30 @@
+package com.peciatech.alomediabackend.auth.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+
+    private final JavaMailSender mailSender;
+
+    @Value("${app.recovery-base-url}")
+    private String recoveryBaseUrl;
+
+    // I did create an email for sending all the recovery emails...
+    // let's hope it works and doesn't end up in spam :D
+    public void sendPasswordRecoveryEmail(String toEmail, String token) {
+        String link = recoveryBaseUrl + "?token=" + token;
+
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(toEmail);
+        message.setSubject("Password Recovery Request");
+        message.setText("Click the link to reset your password: " + link);
+
+        mailSender.send(message);
+    }
+}
