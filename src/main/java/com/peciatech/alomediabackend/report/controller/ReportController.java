195cfca73ab7a9521d4ebdfd3b10fd3586a4bd87
Jud1714 <juandiegolopezarias07@gmail.com>
2026-03-21 10:53:38 -0500

    Update ReportController request mapping to /admin/reports for admin access

--- src/main/java/com/peciatech/alomediabackend/report/controller/ReportController.java
+++ src/main/java/com/peciatech/alomediabackend/report/controller/ReportController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/reports")
+@RequestMapping("/admin/reports")
 @RequiredArgsConstructor
 @PreAuthorize("hasRole('ADMIN')")
 public class ReportController {
