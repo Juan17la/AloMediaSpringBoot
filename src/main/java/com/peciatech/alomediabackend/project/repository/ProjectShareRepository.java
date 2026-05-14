74c0268f791584adf60919909819b6e92aaf7f6f
jefer02 <jefersonmorillo5858@gmail.com>
2026-05-14 15:45:07 -0500

    Add queries to ProjectRepository and ProjectShareRepository for ProjectSummaryResponse

--- src/main/java/com/peciatech/alomediabackend/project/repository/ProjectShareRepository.java
+++ src/main/java/com/peciatech/alomediabackend/project/repository/ProjectShareRepository.java
@@ -1,5 +1,6 @@
 package com.peciatech.alomediabackend.project.repository;
 
+import com.peciatech.alomediabackend.project.dto.response.ProjectSummaryResponse;
 import com.peciatech.alomediabackend.project.entity.Project;
 import com.peciatech.alomediabackend.project.entity.ProjectShare;
 import org.springframework.data.domain.Page;
@@ -16,4 +17,9 @@ public interface ProjectShareRepository extends JpaRepository<ProjectShare, Long
 
     @Query("SELECT ps.project FROM ProjectShare ps WHERE ps.sharedWith.id = :userId")
     Page<Project> findSharedProjectsByUserId(@Param("userId") Long userId, Pageable pageable);
+
+    @Query("SELECT new com.peciatech.alomediabackend.project.dto.response.ProjectSummaryResponse(" +
+           "p.id, p.name, p.status, p.owner.id, p.createdAt, p.updatedAt) " +
+           "FROM ProjectShare ps JOIN ps.project p WHERE ps.sharedWith.id = :userId")
+    Page<ProjectSummaryResponse> findSharedProjectSummariesByUserId(@Param("userId") Long userId, Pageable pageable);
 }
